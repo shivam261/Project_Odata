deb3705f1e6c37f25162ffddd6c67529bf51212e
shivam <shivam1705of@gmail.com>
2025-10-08 03:49:31 +0530

    feat: made other folders

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 from pydantic import BaseModel, EmailStr
 from typing import List, Optional
 from datetime import datetime
 from enum import Enum
 from uuid import UUID, uuid4
+from orjson import dumps
 
 
-app = FastAPI()
+class OrsonResponse(Response):
+    media_type = "application/octet-stream"
+
+    def render(self, content: any) -> bytes:
+        return dumps(content)  # Serialize using Orson
+
+
+app = FastAPI(default_response_class=OrsonResponse)
 
 
 @app.get("/")
